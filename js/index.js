6f084e3b7bcaad59316caf1d712e68975dd29f02
Xavier Arbour <119866415+IshtarFolo@users.noreply.github.com>
2024-10-02 10:41:30 -0400

    Ajout d'un bouton light/ dark mode et du Javascript

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
-/* 
+/*===========================================================
     Fonction pour envoyer le logo en haut à droite et faire
     apparaître le contenu du header.
-*/
+============================================================*/
 let aJoue = false;
 
 // Fonction pour changer la position du logo
@@ -16,7 +16,8 @@ window.addEventListener('scroll', function() {
 
     if (distanceFromTop >= 50 && !aJoue) 
     {
-        if (mediaQuery.matches) {
+        if (mediaQuery.matches) 
+        {
             // Le logo
             logo.style.position = 'static';
             logo.style.transform = 'translate(-10%, 10%)';
@@ -33,8 +34,9 @@ window.addEventListener('scroll', function() {
             nav.style.opacity = '1';
 
             aJoue = true;
-
-        } else {
+        } 
+        else 
+        {
             // Le logo
             logo.style.position = 'static';
             logo.style.transform = 'translate(0%, 0%)';
@@ -52,10 +54,10 @@ window.addEventListener('scroll', function() {
 });
 
 
-/*
+/*============================================================
     Fonction pour faire disparaître le texte des sections
     quand le header arrive à la hauteur du texte.
-*/
+==============================================================*/
 window.addEventListener('scroll', function() {
 
     const sections = document.querySelectorAll('section');
@@ -66,17 +68,20 @@ window.addEventListener('scroll', function() {
         const distanceFromTopSection = section.offsetTop;
         const distanceFromTopHeader = header.offsetTop;
 
-        if (distanceFromTop >= distanceFromTopSection - 100) {
+        if (distanceFromTop >= distanceFromTopSection - 100) 
+        {
             section.style.opacity = '0';
-        } else {
+        } 
+        else 
+        {
             section.style.opacity = '1';
         }
     });
 });
 
-/* 
+/* ==================================================
     Évènements pour le menu burger (ouvert/ fermé) 
-*/
+====================================================*/
 const burgerMenu = document.querySelector('.burger-menu');
 const nav = document.querySelector('nav');
 const navLiens = document.querySelectorAll('nav a');
@@ -88,4 +93,29 @@ burgerMenu.addEventListener('click', function() {
     // On active la classe qui change l'icone du bouton burger
     burgerMenu.classList.toggle('active');
     });
+
+
+/*==================================================================
+    Évènement pour mettre le "ligh mode" ou revenir au "dark mode"
+===================================================================*/
+const bod = document.querySelector('body');
+const btn_Bck = document.querySelector('.DL_Md');
+const point = document.querySelector('.dot');
+
+// Si on clique sur le bouton le point se déplace et on active/ désactive le light ou dark mode
+btn_Bck.addEventListener('click', function() {
+    
+    if (bod.classList.contains('lightMd'))
+    {
+        bod.classList.remove('lightMd');
+        point.style.transform = 'translate(-20px, 0)';
+    }
+    else
+    {
+        bod.classList.add('lightMd');
+        point.style.transform = 'translate(20px, 0)';
+    }
+});
+
+
 
