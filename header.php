3de9689610f6a6245d71ed5f0f151e1748b2e77f
Gomez Ibanez <e2035933@cmaisonneuve.qc.ca>
2024-10-30 10:25:15 -0400

    first commit

---
+++ header.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Projet Web en équipe 5W5</title>
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/normalize.css">
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/style.css">>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Titillium+Web:ital,wght@0,200;0,300;0,400;0,600;0,700;0,900;1,200;1,300;1,400;1,600;1,700&display=swap"
+        rel="stylesheet">
+</head>
+
+<body>
+    <div id="main-content">
+        <div id="container3D"></div>
+
+        <!-- Le header -->
+        <header>
+            <div class="logo-titre">
+                <!-- À VOIR SI ON GARDE LE LOGO -->
+                <img src="images/LogoTim.png" alt="logo-TIM" class="logo">
+                <h1>Intégration Multimédia</h1>
+            </div>
+
+            <!-- Btn Dark/ Light mode-->
+            <div class="DL_Md">
+                <div class="dot"></div>
+            </div>
+            <!-- wooooooooooooooooooo -->
+            <!-- Btn burger -->
+            <div class="burger-menu">
+                <div class="ligne"></div>
+                <div class="ligne"></div>
+                <div class="ligne"></div>
+            </div>
+
+            <!-- Navigation -->
+            <nav>
+                <ul>
+                    <li><a href="">Projets</a></li>
+                    <li><a href="">Cours</a></li>
+                    <li><a href="">Avenir</a></li>
+                    <li><a href="">Vie étudiante</a></li>
+                </ul>
+            </nav>
+        </header>
---
+++ index.php
@@ -0,0 +1,145 @@
+<?php get_header() ?>
+        <!-- Contenu du site -->
+        <main>
+
+            <!-- Texte d'intro TIM -->
+            <section class="intro effacer">
+                <h2>TIM... C'est quoi?</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus ipsa beatae cupiditate eaque a
+                    impedit et dolore, quod libero non corrupti officia magni adipisci inventore ea distinctio iusto
+                    nemo necessitatibus.
+                    Exercitationem cum, iste officiis voluptatibus repudiandae vitae repellendus ex soluta accusantium
+                    cumque quod minima doloribus. Deserunt molestiae dolorum nesciunt repudiandae quae quod in facere
+                    quos, optio voluptatum rerum odit culpa?</p>
+            </section>
+
+            <!-- Apprentissages -->
+            <section class="apprt effacer">
+                <h2>Apprentissages</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium quod repudiandae tempora, error
+                    nobis eos. Quas, nostrum! Similique totam asperiores laboriosam quasi culpa, ex neque, perferendis,
+                    maiores nostrum voluptates sed.</p>
+            </section>
+
+            <!-- Carousel -->
+            <section class="carousel">
+                <h2>Projets en vedette</h2>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quae rerum dignissimos aliquam dolores
+                    pariatur voluptatem voluptates ea odit est quaerat similique ullam nam, maxime id corrupti? Quidem
+                    id tempore laboriosam.</p>
+
+                <!-- Prototype de Carousel -->
+                <div class="carousel">
+                    <figure id="spinner">
+                        <!-- Carte 1 -->
+                        <div class="flip-card no1">
+                            <div class="inner-flip">
+                                <div class="front-flip">
+                                    <img src="https://via.placeholder.com/150" alt="Image 1" class="image">
+                                </div>
+                                <div class="flip-card-back">
+                                    <h1>Carte Ici!</h1>
+                                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis, ...</p>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- Carte 2 -->
+                        <div class="flip-card no2">
+                            <div class="inner-flip">
+                                <div class="front-flip">
+                                    <img src="https://via.placeholder.com/150" alt="Image 2" class="image">
+                                </div>
+                                <div class="flip-card-back">
+                                    <h1>Carte Ici!</h1>
+                                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis, ...</p>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- Carte 3 -->
+                        <div class="flip-card no3">
+                            <div class="inner-flip">
+                                <div class="front-flip">
+                                    <img src="https://via.placeholder.com/150" alt="Image 3" class="image">
+                                </div>
+                                <div class="flip-card-back">
+                                    <h1>Carte Ici!</h1>
+                                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis, ...</p>
+                                </div>
+                            </div>
+                        </div>
+
+                        <!-- Carte 4 -->
+                        <div class="flip-card no4">
+                            <div class="inner-flip">
+                                <div class="front-flip">
+                                    <img src="https://via.placeholder.com/150" alt="Image 4" class="image">
+                                </div>
+                                <div class="flip-card-back">
+                                    <h1>Carte Ici!</h1>
+                                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis, ...</p>
+                                </div>
+                            </div>
+                        </div>
+
+                        <!-- Carte 5 -->
+                        <div class="flip-card no5">
+                            <div class="inner-flip">
+                                <div class="front-flip">
+                                    <img src="https://via.placeholder.com/150" alt="Image 5" class="image">
+                                </div>
+                                <div class="flip-card-back">
+                                    <h1>Carte Ici!</h1>
+                                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis, ...</p>
+                                </div>
+                            </div>
+                        </div>
+
+                        <!-- Carte 6 -->
+                        <div class="flip-card no6">
+                            <div class="inner-flip">
+                                <div class="front-flip">
+                                    <img src="https://via.placeholder.com/150" alt="Image 6" class="image">
+                                </div>
+                                <div class="flip-card-back">
+                                    <h1>Carte Ici!</h1>
+                                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis, ...</p>
+                                </div>
+                            </div>
+                        </div>
+
+                        <!-- Carte 7 -->
+                        <div class="flip-card no7">
+                            <div class="inner-flip">
+                                <div class="front-flip">
+                                    <img src="https://via.placeholder.com/150" alt="Image 7" class="image">
+                                </div>
+                                <div class="flip-card-back">
+                                    <h1>Carte Ici!</h1>
+                                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis, ...</p>
+                                </div>
+                            </div>
+                        </div>
+
+                        <!-- Carte 8 -->
+                        <div class="flip-card no8">
+                            <div class="inner-flip">
+                                <div class="front-flip">
+                                    <img src="https://via.placeholder.com/150" alt="Image 8" class="image">
+                                </div>
+                                <div class="flip-card-back">
+                                    <h1>Carte Ici!</h1>
+                                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis, ...</p>
+                                </div>
+                            </div>
+                        </div>
+                    </figure>
+                </div>
+
+                <div id="boutons">
+                    <!-- Boutons -->
+                    <button class="prev">Prev</button>
+                    <button class="next">Next</button>
+                </div>
+            </section>
+        </main>
+        <?php get_footer(); ?>
