35931aab1f86698737c30ebc54e27b64b02ae743
Xavier Arbour <119866415+IshtarFolo@users.noreply.github.com>
2024-11-13 09:56:34 -0500

    Mod de la page vie étudiante

--- template-etudiant.php
+++ template-etudiant.php
@@ -6,5 +6,22 @@
 ?>
 <?php get_header(); ?>
 
+<?php
+			
+// Récupération de la page vie étudiante
+$page = get_page_by_title('VIE ÉTUDIANTE');
+
+if ($page) {
+    echo 'h1'. get_the_title() . '</h1>';
+    $contenu = apply_filters('the_content', $page->post_content);
+
+    // Ajout du contenu de la page vie étudiante
+    echo $contenu;
+} else {
+    echo '<p>Page "VIE ÉTUDIANTE" not found.</p>';
+}
+        
+
+?>
 
 <?php get_footer(); ?>
