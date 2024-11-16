630700bc6d3c347a881f2f9e711660dfb154a514
ChiroJoules <julcamgo2003@gmail.com>
2024-11-16 15:39:29 -0500

    ajout page single, style de la page et modif sur la page 404

---
+++ single.php
@@ -0,0 +1,28 @@
+<?php get_header() ?>
+<section class="singleSection">
+    <div class="contenuSingle">
+        <?php if(have_posts()): the_post() ?>
+            <h2><?= the_title(); ?></h2>
+            <div class="acfSingle">
+                <?php 
+                    if (has_category('profs')) : 
+                ?>
+                    <h3>Type: <?php the_field('type'); ?></h3>
+                    <h3>Cours: <?php the_field('cours'); ?></h3>
+                <?php 
+                    elseif (has_category('cours')) : 
+                ?>
+                    <h3>Heures au total: <?php the_field('Heures'); ?>h</h3>
+                    <h3>Ponderation: <?php the_field('poderation'); ?></h3>
+                <?php 
+                    endif; 
+                ?>
+            </div>
+            <div id="photoSingle">
+                <?= the_post_thumbnail('medium'); ?>
+            </div>
+            <p><?= the_content(); ?></p>
+            <?php endif; ?>     
+    </div>
+</section>
+<?php get_footer(); ?>
