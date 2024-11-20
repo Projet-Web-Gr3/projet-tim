a88c19b07716ba2cf0ab569a51eb1e92c2db93fa
GameCube078 <119693826+GameCube078@users.noreply.github.com>
2024-11-20 11:44:25 -0500

    Changement des images apprentissage en whc

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php get_header() ?>
 <div id="main-content">
         <div id="container3D"></div>
+        <div id="blobCouleurs"></div>
     <!-- Contenu du site -->
         <main>   
             <section class="accueil_effacer">
@@ -20,11 +21,44 @@
             </section>
 
             <!-- Apprentissages -->
-            <section class="apprt effacer">
-                <h2>Apprentissages</h2>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium quod repudiandae tempora, error
-                    nobis eos. Quas, nostrum! Similique totam asperiores laboriosam quasi culpa, ex neque, perferendis,
-                    maiores nostrum voluptates sed.</p>
+            <section class="apprentissages effacer">
+                <div class="jeuApprentissage">
+                    <h2>JEU</h2>
+                    <Div>
+                        <img class="img1Effet" src="https://gftnth00.mywhc.ca/tim17/wp-content/uploads/2024/11/Unity.png" alt="Unity">
+                        <img class="img2Effet" src="https://gftnth00.mywhc.ca/tim17/wp-content/uploads/2024/11/CSharp.png" alt="CSharp">
+                        <img class="img3Effet" src="https://gftnth00.mywhc.ca/tim17/wp-content/uploads/2024/11/Meta.png" alt="meta">
+                        <img class="img4Effet" src="https://gftnth00.mywhc.ca/tim17/wp-content/uploads/2024/11/VS.png" alt="VS">
+                    </Div>
+                </div>
+                <div class="audiovisuelApprentissage">
+                    <h2>AUDIOVISUEL</h2>
+                    <Div>
+                        <img class="img1Effet" src="https://gftnth00.mywhc.ca/tim17/wp-content/uploads/2024/11/Ae.png" alt="Ae">
+                        <img class="img2Effet" src="https://gftnth00.mywhc.ca/tim17/wp-content/uploads/2024/11/Maya.png" alt="Maya">
+                        <img class="img3Effet" src="https://gftnth00.mywhc.ca/tim17/wp-content/uploads/2024/11/Pt.png" alt="Pt">
+                        <img class="img4Effet" src="https://gftnth00.mywhc.ca/tim17/wp-content/uploads/2024/11/TouchDesigner.png" alt="TouchDesigner">
+                    </Div>
+                </div>
+                <div class="designApprentissage">
+                    <h2>DESIGN</h2>
+                    <Div>
+                        <img class="img1Effet" src="https://gftnth00.mywhc.ca/tim17/wp-content/uploads/2024/11/Ps.png" alt="Ps">
+                        <img class="img2Effet" src="https://gftnth00.mywhc.ca/tim17/wp-content/uploads/2024/11/Ai.png" alt="Ai">
+                        <img class="img3Effet" src="https://gftnth00.mywhc.ca/tim17/wp-content/uploads/2024/11/Figma.png" alt="Figma">
+                        <img class="img4Effet" src="https://gftnth00.mywhc.ca/tim17/wp-content/uploads/2024/11/Xd.png" alt="XD">
+                    </Div>
+                </div>
+                <div class="webApprentissage">
+                    <h2>WEB</h2>
+                    <Div>
+                        <img class="img1Effet" src="https://gftnth00.mywhc.ca/tim17/wp-content/uploads/2024/11/HTML.png" alt="HTML">
+                        <img class="img2Effet" src="https://gftnth00.mywhc.ca/tim17/wp-content/uploads/2024/11/CSS.png" alt="CSS">
+                        <img class="img3Effet" src="https://gftnth00.mywhc.ca/tim17/wp-content/uploads/2024/11/Javascript.png" alt="Javascript">
+                        <img class="img4Effet" src="https://gftnth00.mywhc.ca/tim17/wp-content/uploads/2024/11/Github.png" alt="Github">
+                    </Div>
+                </div>
+
             </section>
 
             <!-- Carousel -->
