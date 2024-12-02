e91225e01cf023ac32314da85c147614a468fa5f
Xavier Arbour <arbour.xavier34@gmail.com>
2024-12-02 09:16:32 -0500

    Carousel fonctionnel dans la page d'acceuil

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
         <main>   
             <section class="accueil_effacer">
                 <h1>BIENVENUE AU</h1>
-                <img src="/wordpress/wp-content/themes/projet-tim/images/LogoTIMHiRes.png" alt="" class="logoGrand">
+                <img src="https://gftnth00.mywhc.ca/tim17/wp-content/uploads/2024/11/LogoTIMHiRes.png" alt="" class="logoGrand">
                 <h1>TECHNIQUE D'INTÉGRATION MULTIMÉDIA</h1>
             </section>
             <!-- Texte d'intro TIM -->
@@ -19,133 +19,53 @@
                     quos, optio voluptatum rerum odit culpa?</p>
             </section>
 
-            <!-- Apprentissages -->
-            <section class="apprt effacer">
-                <h2>Apprentissages</h2>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium quod repudiandae tempora, error
-                    nobis eos. Quas, nostrum! Similique totam asperiores laboriosam quasi culpa, ex neque, perferendis,
-                    maiores nostrum voluptates sed.</p>
+           <!-- Apprentissages -->
+            <section class="apprentissages effacer">
+                <div class="jeuApprentissage">
+                    <h2>JEU</h2>
+                    <Div>
+                        <img class="img1Effet" src="https://gftnth00.mywhc.ca/tim17/wp-content/uploads/2024/11/Unity.png" alt="Unity">
+                        <img class="img2Effet" src="https://gftnth00.mywhc.ca/tim17/wp-content/uploads/2024/11/CSharp.png" alt="CSharp">
+                        <img class="img3Effet" src="https://gftnth00.mywhc.ca/tim17/wp-content/uploads/2024/11/Meta.png" alt="meta">
+                        <img class="img4Effet" src="https://gftnth00.mywhc.ca/tim17/wp-content/uploads/2024/11/VS.png" alt="VS">
+                    </Div>
+                </div>
+                <div class="audiovisuelApprentissage">
+                    <h2>AUDIOVISUEL</h2>
+                    <Div>
+                        <img class="img1Effet" src="https://gftnth00.mywhc.ca/tim17/wp-content/uploads/2024/11/Ae.png" alt="Ae">
+                        <img class="img2Effet" src="https://gftnth00.mywhc.ca/tim17/wp-content/uploads/2024/11/Maya.png" alt="Maya">
+                        <img class="img3Effet" src="https://gftnth00.mywhc.ca/tim17/wp-content/uploads/2024/11/Pt.png" alt="Pt">
+                        <img class="img4Effet" src="https://gftnth00.mywhc.ca/tim17/wp-content/uploads/2024/11/TouchDesigner.png" alt="TouchDesigner">
+                    </Div>
+                </div>
+                <div class="designApprentissage">
+                    <h2>DESIGN</h2>
+                    <Div>
+                        <img class="img1Effet" src="https://gftnth00.mywhc.ca/tim17/wp-content/uploads/2024/11/Ps.png" alt="Ps">
+                        <img class="img2Effet" src="https://gftnth00.mywhc.ca/tim17/wp-content/uploads/2024/11/Ai.png" alt="Ai">
+                        <img class="img3Effet" src="https://gftnth00.mywhc.ca/tim17/wp-content/uploads/2024/11/Figma.png" alt="Figma">
+                        <img class="img4Effet" src="https://gftnth00.mywhc.ca/tim17/wp-content/uploads/2024/11/Xd.png" alt="XD">
+                    </Div>
+                </div>
+                <div class="webApprentissage">
+                    <h2>WEB</h2>
+                    <div>
+                        <img class="img1Effet" src="https://gftnth00.mywhc.ca/tim17/wp-content/uploads/2024/11/HTML.png" alt="HTML">
+                        <img class="img2Effet" src="https://gftnth00.mywhc.ca/tim17/wp-content/uploads/2024/11/CSS.png" alt="CSS">
+                        <img class="img3Effet" src="https://gftnth00.mywhc.ca/tim17/wp-content/uploads/2024/11/Javascript.png" alt="Javascript">
+                        <img class="img4Effet" src="https://gftnth00.mywhc.ca/tim17/wp-content/uploads/2024/11/Github.png" alt="Github">
+                    </div>
+                </div>
+
             </section>
 
             <!-- Carousel -->
             <section class="carousel">
-                <h2>Projets en vedette</h2>
-                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quae rerum dignissimos aliquam dolores
-                    pariatur voluptatem voluptates ea odit est quaerat similique ullam nam, maxime id corrupti? Quidem
-                    id tempore laboriosam.</p>
-
-                <!-- Prototype de Carousel -->
-                <div class="carousel">
-                    <figure id="spinner">
-                        <!-- Carte 1 -->
-                        <div class="flip-card no1">
-                            <div class="inner-flip">
-                                <div class="front-flip">
-                                    <img src="https://via.placeholder.com/150" alt="Image 1" class="image">
-                                </div>
-                                <div class="flip-card-back">
-                                    <h1>Carte Ici!</h1>
-                                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis, ...</p>
-                                </div>
-                            </div>
-                        </div>
-                        <!-- Carte 2 -->
-                        <div class="flip-card no2">
-                            <div class="inner-flip">
-                                <div class="front-flip">
-                                    <img src="https://via.placeholder.com/150" alt="Image 2" class="image">
-                                </div>
-                                <div class="flip-card-back">
-                                    <h1>Carte Ici!</h1>
-                                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis, ...</p>
-                                </div>
-                            </div>
-                        </div>
-                        <!-- Carte 3 -->
-                        <div class="flip-card no3">
-                            <div class="inner-flip">
-                                <div class="front-flip">
-                                    <img src="https://via.placeholder.com/150" alt="Image 3" class="image">
-                                </div>
-                                <div class="flip-card-back">
-                                    <h1>Carte Ici!</h1>
-                                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis, ...</p>
-                                </div>
-                            </div>
-                        </div>
-
-                        <!-- Carte 4 -->
-                        <div class="flip-card no4">
-                            <div class="inner-flip">
-                                <div class="front-flip">
-                                    <img src="https://via.placeholder.com/150" alt="Image 4" class="image">
-                                </div>
-                                <div class="flip-card-back">
-                                    <h1>Carte Ici!</h1>
-                                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis, ...</p>
-                                </div>
-                            </div>
-                        </div>
-
-                        <!-- Carte 5 -->
-                        <div class="flip-card no5">
-                            <div class="inner-flip">
-                                <div class="front-flip">
-                                    <img src="https://via.placeholder.com/150" alt="Image 5" class="image">
-                                </div>
-                                <div class="flip-card-back">
-                                    <h1>Carte Ici!</h1>
-                                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis, ...</p>
-                                </div>
-                            </div>
-                        </div>
-
-                        <!-- Carte 6 -->
-                        <div class="flip-card no6">
-                            <div class="inner-flip">
-                                <div class="front-flip">
-                                    <img src="https://via.placeholder.com/150" alt="Image 6" class="image">
-                                </div>
-                                <div class="flip-card-back">
-                                    <h1>Carte Ici!</h1>
-                                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis, ...</p>
-                                </div>
-                            </div>
-                        </div>
-
-                        <!-- Carte 7 -->
-                        <div class="flip-card no7">
-                            <div class="inner-flip">
-                                <div class="front-flip">
-                                    <img src="https://via.placeholder.com/150" alt="Image 7" class="image">
-                                </div>
-                                <div class="flip-card-back">
-                                    <h1>Carte Ici!</h1>
-                                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis, ...</p>
-                                </div>
-                            </div>
-                        </div>
-
-                        <!-- Carte 8 -->
-                        <div class="flip-card no8">
-                            <div class="inner-flip">
-                                <div class="front-flip">
-                                    <img src="https://via.placeholder.com/150" alt="Image 8" class="image">
-                                </div>
-                                <div class="flip-card-back">
-                                    <h1>Carte Ici!</h1>
-                                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis, ...</p>
-                                </div>
-                            </div>
-                        </div>
-                    </figure>
-                </div>
-
-                <div id="boutons">
-                    <!-- Boutons -->
-                    <button class="prev">Prev</button>
-                    <button class="next">Next</button>
-                </div>
-            </section>
+				<?php
+				include_once($_SERVER['DOCUMENT_ROOT'].'/wp-load.php');
+				echo do_shortcode('[carousel]');
+				?>
+			</section>
         </main>
         <?php get_footer(); ?>
